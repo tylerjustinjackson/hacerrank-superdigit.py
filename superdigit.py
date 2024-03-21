b44e2ba1159426557a6e9ac62477acbf57af1591
Tyler Jackson <109995249+tylerjustinjackson@users.noreply.github.com>
2024-03-20 23:05:31 -0500

    Update superdigit.py

--- superdigit.py
+++ superdigit.py
@@ -1,12 +1,26 @@
 #!/bin/python3
 
+#super digit main function
 def super_digit(n, k):
+
+    #makes all the number integer from n list
     digits = map(int, list(n))
+    
+    #calls get super digit secondary function and
+    #passes the digits to the function
     return get_super_digit(str(sum(digits) * k))
 
+#define get super digit function
 def get_super_digit(p):
+
+    #finds lenght of p and sees if it is 1
     if len(p) == 1:
+
+        #returns p if length is 1
         return int(p)
+
+    #otherwise maps all digits back into integers
+    #and restarts process
     else:
         digits = map(int, list(p))
         return get_super_digit(str(sum(digits)))
